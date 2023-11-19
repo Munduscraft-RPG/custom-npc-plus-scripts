79813232ca4e155262ac314f22977d4a2fbdd851
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 18:58:56 -0500

    Clean up cleareffect command

--- Commands/cleareffect.js
+++ Commands/cleareffect.js
@@ -2,11 +2,11 @@ function cleareffect(player,args){
     switch(args.length){
         case 0:
             player.clearPotionEffects();
-            player.sendMessage(STYLE_SUCCESS+'Potion effects cleared ');
+            player.sendMessage(STYLE_SUCCESS+'Potion effects cleared');
             return;
         case 1:
             var targetPlayer = getPlayerByName(args[0]);
-            player.sendMessage(STYLE_SUCCESS+'Potion effects cleared for target ' + args[0]);
+            player.sendMessage(STYLE_SUCCESS+'Potion effects cleared for ' + args[0]);
             targetPlayer.clearPotionEffects();
             return;
         default:
