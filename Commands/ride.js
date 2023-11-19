61dbf6dfe3de4de6f9765a36652a8254bbbff25a
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-18 22:37:55 -0500

    Fix ride command args check

--- Commands/ride.js
+++ Commands/ride.js
@@ -1,7 +1,7 @@
 function ride(player, args) {
 
     // Check Arguments
-    if(args !== 0) { player.sendMessage(STYLE_RESET+'Invalid usage of ?ride command.'); return; }
+    if(args.length !== 0) { player.sendMessage(STYLE_RESET+'Invalid usage of ?ride command.'); return; }
 
     // Get looking at entity
     var lookingAt = player.getLookingAtEntities(3, 0, 0.5, true, true, true)
