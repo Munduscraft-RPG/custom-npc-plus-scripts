5e0e168a9ae724545eef50876fc3d7dc2cfa3229
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 18:56:50 -0500

    Clean up broadcast command

--- Commands/broadcast.js
+++ Commands/broadcast.js
@@ -1,13 +1,18 @@
 function broadcast(player, args) {
-    if(args.length === 0) {throw "invalid_usage";}
-    // Get all players
-    playerList = API.getAllServerPlayers();
+    switch(args.length) {
+        case 1:
+            // Get all players
+            var playerList = API.getAllServerPlayers();
 
-    // For each player
-    for(var i = 0; i<playerList.length; i++) {
-        // Send broadcast message
-        playerList[i].sendMessage('&e&lBroadcast: &r'+args.join(' '));
+            // For each player
+            for(var i = 0; i<playerList.length; i++) {
+                // Send broadcast message
+                playerList[i].sendMessage('&e&lBroadcast: &r'+args.join(' '));
+            }
+
+            return;
+
+        default:
+            throw "invalid_usage";
     }
-
-    return;
 }
