1a01f872f0bc9b43575d44b936cef9b98f2dfd7d
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 18:51:10 -0500

    Clean up admin command

--- Commands/admin.js
+++ Commands/admin.js
@@ -1,25 +1,26 @@
 function admin(player, args) {
 
-    // First argument should be 'list' 'add' or 'remove'
+    // Get admin list
+    var adminList = JSON.parse(API.getIWorld(0).getStoredData('adminList') || '[]');
+
+    // First argument must be 'list' 'add' or 'remove'
     switch(args[0]) {
         case 'list':
 
             // Check args length
             if(args.length !== 1) {throw "invalid_usage";}
 
-            // Get admin list
-            var adminListStr = API.getIWorld(0).getStoredData('adminList');
-            var adminList = JSON.parse(adminListStr || '[]');
-
             // Check if admin list is empty
             if(adminList.length > 0) {
-                //Send admin list
+
+                //Send admin list message
                 player.sendMessage(STYLE_SUCCESS+"Admin List:")
                 for(var i = 0; i < adminList.length; i++) {
-                    player.sendMessage(STYLE_SUCCESS+eval(i+1)+". "+adminList[i]);
+                    player.sendMessage(STYLE_SUCCESS+(i+1)+". "+adminList[i]);
                 }
 
             } else {
+
                 // No admins found
                 player.sendMessage(STYLE_FAILURE+"No admins found!")
             }
@@ -33,13 +34,13 @@ function admin(player, args) {
             // Check player is valid
             getPlayerByName(args[1]);
 
-            // Update admin list
-            var adminListStr = API.getIWorld(0).getStoredData('adminList');
-            var adminList = JSON.parse(adminListStr || '[]');
+            // Update adminList
             if (adminList.indexOf(args[1]) === -1) {
                 adminList.push(args[1]);
                 API.getIWorld(0).setStoredData('adminList', JSON.stringify(adminList));
             }
+
+            // Send message
             player.sendMessage(STYLE_SUCCESS + 'Successfully added admin: ' + args[1]);
             break;
 
@@ -52,13 +53,13 @@ function admin(player, args) {
             getPlayerByName(args[1]);
 
             // Update adminList
-            var adminListStr = API.getIWorld(0).getStoredData('adminList');
-            var adminList = JSON.parse(adminListStr || '[]');
             var index = adminList.indexOf(args[1]);
             if (index > -1) {
                 adminList.splice(index, 1);
                 API.getIWorld(0).setStoredData('adminList', JSON.stringify(adminList));
             }
+
+            // Send message
             player.sendMessage(STYLE_SUCCESS + 'Successfully removed admin: ' + args[1]);
             break;
 
