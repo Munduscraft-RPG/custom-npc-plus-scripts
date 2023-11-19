f1a26cdbf9d7f62dc2f9302aee16a9638b47cd45
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 14:30:47 -0500

    feat(commandParser): Add invalid usage check
    Use try catch to catch invalid_usage errors instead of using return and print in every command.

--- commandParser.js
+++ commandParser.js
@@ -3,44 +3,52 @@ function commandParser(player, message) {
     var command = parts[0];
     var args = parts.slice(1);
 
-    switch (command) {
-        case 'a':
-        case 'admin':
-            admin(player, args);
-            break;
-        case 'b':
-        case 'broadcast':
-            broadcast(player, args);
-            break;
-        case 'ce':
-        case 'cleareffect':
-            cleareffect(player, args);
-            break;
-        case 'n':
-        case 'nick':
-            nick(player, args);
-            break;
-        case 'p':
-        case 'ping':
-            ping(player, args);
-            break;
-        case 'r':
-        case 'reload':
-            reload(player, args);
-            break;
-        case 'ride':
-            ride(player, args);
-            break;
-        case 'tp':
-        case 'teleport':
-            teleport(player, args);
-            break;
-        case 'v':
-        case 'vanish':
-            vanish(player, args);
-            break;
-        default:
-            player.sendMessage(STYLE_FAILURE + 'Unknown command');
-            break;
+    try {
+        switch (command) {
+            case 'a':
+            case 'admin':
+                admin(player, args);
+                break;
+            case 'b':
+            case 'broadcast':
+                broadcast(player, args);
+                break;
+            case 'ce':
+            case 'cleareffect':
+                cleareffect(player, args);
+                break;
+            case 'n':
+            case 'nick':
+                nick(player, args);
+                break;
+            case 'p':
+            case 'ping':
+                ping(player, args);
+                break;
+            case 'r':
+            case 'reload':
+                reload(player, args);
+                break;
+            case 'ride':
+                ride(player, args);
+                break;
+            case 'tp':
+            case 'teleport':
+                teleport(player, args);
+                break;
+            case 'v':
+            case 'vanish':
+                vanish(player, args);
+                break;
+            default:
+                player.sendMessage(STYLE_FAILURE + 'Unknown command');
+                break;
+        }
+    } catch(exception) {
+        if(typeof exception === "string" && exception === "invalid_usage") {
+            player.sendMessage(STYLE_FAILURE+"Invalid usage of ?"+command);
+            return;
+        }
+        throw exception;
     }
 }
