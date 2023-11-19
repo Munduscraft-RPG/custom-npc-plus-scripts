34d1b4d573145e0144314e8c9f2f4f99e9ef87f7
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 13:47:10 -0500

    Convert drunk text to speech modifier function in its own file to allow for other speech modifiers to be created.

--- chatCustom.js
+++ chatCustom.js
@@ -81,8 +81,8 @@ function chatCustom(event) {
     }
 
     // If the player is drunk, use drunk text distortion
-    if(player.hasStoredData('isDrunk') && player.getStoredData('isDrunk')) {
-        message = drunkDistort(message);
+    if(player.hasStoredData('isTipsy') && player.getStoredData('isTipsy')) {
+        message = speechModifier('tipsy', message);
     }
 
     // Format message into a full chat message
@@ -114,89 +114,4 @@ function chatCustom(event) {
         if(adminList.indexOf(nearbyPlayers[i].getName()) !== -1) { continue; }
         nearbyPlayers[i].sendMessage(message);
     }
-}
-
-// Function that takes in a message and distorts it to be drunk text
-function drunkDistort(message) {
-
-    //List of rules for distorting the text
-    var distortionRules = [
-        { from: "s", to: "sh", probability: 0.9 },
-        { from: "ch", to: "sh", probability: 0.7 },
-        { from: "h", to: "hh", probability: 0.6 },
-        { from: "th", to: "thl", probability: 0.4 },
-        { from: "sch", to: "shk", probability: 0.6 },
-        { from: "u", to: "uuh", probability: 0.2 },
-        { from: "y", to: "yy", probability: 0.6 },
-        { from: "e", to: "ee", probability: 0.4 },
-        { from: "you", to: "u", probability: 0.4 },
-        { from: "u", to: "uo", probability: 0.6 },
-        { from: "that", to: "taht", probability: 0.2 },
-        { from: "them", to: "thems", probability: 0.4 },
-        { from: "p", to: "b", probability: 0.3 },
-        { from: "p", to: "b", probability: 0.7 },
-        { from: "up", to: "ubb", probability: 0.8 },
-        { from: "o", to: "oh", probability: 0.2 },
-        { from: "ei", to: "i", probability: 0.3 },
-        { from: "b", to: "bb", probability: 0.8 },
-        { from: "!!!", to: "!!!111!!!eleven!1!", probability: 0.5 },
-        { from: "!", to: "!!", probability: 0.9 },
-        { from: "drunk", to: "dhrkunn", probability: 0.7 },
-        { from: "walk", to: "whhealhk", probability: 0.8 },
-        { from: "wtf", to: "wft", probability: 0.2 },
-        { from: "lol", to: "loool", probability: 0.8 },
-        { from: "afk", to: "aafkayyy", probability: 0.3 },
-        { from: "write", to: "wreitt", probability: 0.8 },
-        { from: "drink", to: "booze", probability: 0.8 },
-        { from: "like", to: "LOVE", probability: 0.9},
-        { from: "want", to: "NEED", probability: 0.9},
-        { from: "?", to: "????", probability: 0.8 },
-        { from: ":)", to: ";)", probability: 1.0},
-        { from: ":D", to: ";)", probability: 1.0},
-        { from: ":-)", to: "8==D", probability: 1.0},
-        { from: ":3", to: ";3", probability: 1.0},
-        { from: " ", to: " *hic* ", probability: 0.2}
-    ];
-
-    // Start building the distorted message
-    var distortedMessage = '';
-
-    // For each charectar in the message
-    var i = 0;
-    while (i < message.length) {
-        var replaced = false;
-
-        // See if any rules apply and apply them
-        for (var j = 0; j < distortionRules.length; j++) {
-
-            var rule = distortionRules[j];
-            var fromLength = rule.from.length;
-            var messageSubstring = message.substr(i, fromLength).toLowerCase();
-            var ruleFromLower = rule.from.toLowerCase();
-
-            // If a rule does apply, apply it
-            if (messageSubstring === ruleFromLower && Math.random() < rule.probability) {
-
-                // Make change
-                distortedMessage += rule.to;
-
-                // Skip however many charectars are needed to get to the next unmodified charectar
-                i += fromLength;
-
-                replaced = true;
-                break;
-            }
-        }
-        // If nothing is changed
-        if (!replaced) {
-
-            // Do not change anything
-            distortedMessage += message[i];
-
-            // Move to next charectar
-            i++;
-        }
-    }
-
-    return distortedMessage;
 }
