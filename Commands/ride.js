10406db492831bc124ae98e4a10ae48a27487137
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 19:07:13 -0500

    Clean up reload command

--- Commands/ride.js
+++ Commands/ride.js
@@ -1,12 +1,17 @@
 function ride(player, args) {
 
-    // Check Arguments
-    if(args.length !== 0) {throw "invalid_usage";}
+    switch(args.length) {
+        case 0:
 
-    // Get looking at entity
-    var lookingAt = player.getLookingAtEntities(3, 0, 0.5, true, true, true)
-    if(lookingAt.length === 0) { return; }
+            // Get looking at entity
+            var lookingAt = player.getLookingAtEntities(3, 0, 0.5, true, true, true)
+            if(lookingAt.length === 0) { return; }
 
-    // Ride it
-    player.setMount(lookingAt[0])
+            // Ride it
+            player.setMount(lookingAt[0])
+            return;
+
+        default:
+            throw "invalid_usage";
+    }
 }
