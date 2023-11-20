5682c4041006797241cf19161566849c044d554f
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 19:10:29 -0500

    Add target player support to ride command

--- Commands/ride.js
+++ Commands/ride.js
@@ -11,6 +11,19 @@ function ride(player, args) {
             player.setMount(lookingAt[0])
             return;
 
+        case 1:
+
+            // Get target player
+            var targetPlayer = getPlayerByName(args[0]);
+
+            // Get looking at entity
+            var lookingAt = player.getLookingAtEntities(3, 0, 0.5, true, true, true)
+            if(lookingAt.length === 0) { return; }
+
+            // Ride it
+            targetPlayer.setMount(lookingAt[0])
+            return;
+
         default:
             throw "invalid_usage";
     }
