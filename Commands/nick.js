734e26bc6468617cddac117937222974a9d11dd6
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 19:01:47 -0500

    Clean up nick command

--- Commands/nick.js
+++ Commands/nick.js
@@ -1,14 +1,33 @@
 function nick(player, args) {
-    if (args.length === 1) {
-        var newNick = args[0].replace(/_/g, ' ');
-        player.setStoredData('nickname', newNick);
-        player.sendMessage(STYLE_SUCCESS+'Nickname set to: '+ STYLE_RESET + newNick);
-        return;
-    } else if (args.length === 2) {
-        var targetPlayer = getPlayerByName(args[0]);
-        var targetNick = args[1].replace(/_/g, ' ');
-        targetPlayer.setStoredData('nickname', targetNick);
-        player.sendMessage(STYLE_SUCCESS+'Nickname for '+STYLE_RESET+args[0]+STYLE_SUCCESS+' set to: '+STYLE_RESET+targetNick);
-        return;
-    } else {throw "invalid_usage";}
+    switch(args.length){
+        case 1:
+
+            // Get nickname
+            var newNick = args[0].replace(/_/g, ' ');
+
+            // Store in player data
+            player.setStoredData('nickname', newNick);
+
+            // Send message
+            player.sendMessage(STYLE_SUCCESS+'Nickname set to: '+ STYLE_RESET + newNick);
+
+            return;
+        case 2:
+
+            // Get target player
+            var targetPlayer = getPlayerByName(args[0]);
+
+            // Get nickname
+            var targetNick = args[1].replace(/_/g, ' ');
+
+            // Store in player data
+            targetPlayer.setStoredData('nickname', targetNick);
+
+            // Send message
+            player.sendMessage(STYLE_SUCCESS+'Nickname for '+STYLE_RESET+args[0]+STYLE_SUCCESS+' set to: '+STYLE_RESET+targetNick);
+
+            return;
+        default:
+            throw "invalid_usage";
+    }
 }
