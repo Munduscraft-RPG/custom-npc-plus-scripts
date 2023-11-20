f1510d8e27cb791f719352ca6c72b71c14e977c1
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-19 20:29:39 -0500

    use early returns and define variables in correct scope in teleport.js

--- chat/Commands/teleport.js
+++ chat/Commands/teleport.js
@@ -1,27 +1,31 @@
 function teleport(player, args) {
+
+    var targetPlayer;
+    var victimPlayer;
+
     switch(args.length) {
 
         // One Argument: Teleport self to player
         case 1:
-            var targetPlayer = getPlayerByName(args[0]);
+            targetPlayer = getPlayerByName(args[0]);
             player.sendMessage(STYLE_SUCCESS+'Teleporting to ' + args[0]);
             player.setPosition(targetPlayer.getPosition(), targetPlayer.getDimension());
-            break;
+            return;
 
         // Two Arguments: Teleport victim to target
         case 2:
-            var victimPlayer = getPlayerByName(args[0]);
-            var targetPlayer = getPlayerByName(args[1]);
+            victimPlayer = getPlayerByName(args[0]);
+            targetPlayer = getPlayerByName(args[1]);
             player.sendMessage(STYLE_SUCCESS+'Teleporting '+args[0]+' to '+args[1]);
             victimPlayer.setPosition(targetPlayer.getPosition(), targetPlayer.getDimension());
-            break;
+            return;
 
         // Three arguments: Teleport self to x y z coordinates
         case 3:
             if(!NUMERIC_REGEX.test(args[0]) || !NUMERIC_REGEX.test(args[1]) || !NUMERIC_REGEX.test(args[2])) {throw "invalid_usage";}
             player.sendMessage(STYLE_SUCCESS+'Teleporting to ('+args[0]+','+args[1]+','+args[2]+') in dimension '+player.getDimension());
             player.setPosition(args[0], args[1], args[2], player.getDimension());
-            break;
+            return;
 
         // Four arguments: Teleport self to x y z and dimension id OR teleport victim to x y z
         case 4:
@@ -29,12 +33,12 @@ function teleport(player, args) {
                 try { API.getIWorld(args[3]) } catch (e) { player.sendMessage(STYLE_FAILURE+'Target dimension not found.'); return; }
                 player.sendMessage(STYLE_SUCCESS+'Teleporting to (' + args[0] + ',' + args[1] + ',' + args[2] + ') in dimension ' + args[3]);
                 player.setPosition(args[0], args[1], args[2], args[3])
-                break;
+                return;
             } else if ( !NUMERIC_REGEX.test(args[0]) && NUMERIC_REGEX.test(args[1]) && NUMERIC_REGEX.test(args[2]) && NUMERIC_REGEX.test(args[3]) ) {
-                var victimPlayer = getPlayerByName(args[0]);
+                victimPlayer = getPlayerByName(args[0]);
                 player.sendMessage(STYLE_SUCCESS+'Teleporting '+args[0]+STYLE_SUCCESS+' to (' + args[1] + ',' + args[2] + ',' + args[3] + ') in dimension ' + player.getDimension());
                 victimPlayer.setPosition(args[1], args[2], args[3]);
-                break;
+                return;
             } else {
                 throw "invalid_usage";
             }
@@ -42,14 +46,13 @@ function teleport(player, args) {
         // Five Arguments: Teleport victim to x y z and dimension id
         case 5:
             try { API.getIWorld(args[4]) } catch (e) { player.sendMessage(STYLE_FAILURE+'Target dimension not found.'); return; }
-            var victimPlayer = getPlayerByName(args[0]);
+            victimPlayer = getPlayerByName(args[0]);
             player.sendMessage(STYLE_SUCCESS+'Teleporting '+args[0]+' to (' + args[1] + ',' + args[2] + ',' + args[3] + ') in dimension ' + args[4]);
             victimPlayer.setPosition(args[1], args[2], args[3], args[4]);
-            break;
+            return;
 
         // Other argument count: Invalid
         default:
             throw "invalid_usage";
     }
-    return;
 }
