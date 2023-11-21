6fe5ba037dc6ebeafea9af4d4fb5905746b90398
DylanLogan2581 <github@dylanlogan.xyz>
2023-11-21 10:52:11 -0500

    Add feedback to hat command

--- src/chat/Commands/hat.js
+++ src/chat/Commands/hat.js
@@ -5,10 +5,12 @@ function hat(player, args) {
     switch(args.length) {
         case 0:
             player.setArmor(3, player.getHeldItem());
+            player.sendMessage(STYLE_SUCCESS+'You are now wearing a hat');
             return;
         case 1:
             var targetPlayer = getPlayerByName(args[0]);
             targetPlayer.setArmor(3, player.getHeldItem());
+            player.sendMessage(STYLE_SUCCESS+args[0]+' is now wearing a hat');
             return;
         default:
             throw 'invalid_usage';
