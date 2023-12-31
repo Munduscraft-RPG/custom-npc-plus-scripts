3381d26c01dbdb3f1881060cd4c25ec496abcd40
CamWads <cam.development2@gmail.com>
2023-12-31 06:17:44 -0800

    Added success message to spawn room set command

--- src/chat/Commands/setspawn.js
+++ src/chat/Commands/setspawn.js
@@ -7,4 +7,5 @@ function setspawn(player, args){
     API.getIWorld(0).setStoredData('spawnX',coords[0]);
     API.getIWorld(0).setStoredData('spawnY',coords[1]);
     API.getIWorld(0).setStoredData('spawnZ',coords[2]);
+    player.sendMessage(STYLE_SUCCESS+'Spawn point set');
 }
