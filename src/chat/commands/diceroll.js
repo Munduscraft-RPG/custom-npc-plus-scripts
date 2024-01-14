06ca1579e98cea432f0e65e78af9b59e7aebce9a
DylanLogan2581 <github@dylanlogan.xyz>
2024-01-14 13:37:54 -0500

    Make diceroll more efficient.

--- src/chat/commands/diceroll.js
+++ src/chat/commands/diceroll.js
@@ -1,6 +1,5 @@
 function diceroll(player, args) {
     if(args.length !== 1) { throw 'invalid_usage' }
     if(args[0] <= 0 ) { throw 'invalid_usage' }
-    var result = roll(args[0])
-    player.sendMessage(STYLE_SUCCESS+"Rolled "+result+" on "+args[0]+ " sided die.")
+    player.sendMessage(STYLE_SUCCESS+"Rolled "+roll(args[0])+" on "+args[0]+ " sided die.")
 }
