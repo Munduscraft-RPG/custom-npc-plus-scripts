d0ffaf0965484ddc8511aafd7da57c94b0606d96
CamWads <cam.development2@gmail.com>
2024-01-17 16:55:35 -0800

    Corpse NPC for player KO now has players skin and will despawn when the player respawns

--- src/damaged/damagedPlayer.js
+++ src/damaged/damagedPlayer.js
@@ -2,5 +2,6 @@ function damagedPlayer(event) {
     var player = event.getPlayer();
     var damage = event.getDamage();
     if(player.getHealth() - damage > 0) { return; }
+    event.setCancelled(true);
     koPlayer(player);
 }
--- src/utils/knockout/revivePlayer.js
+++ src/utils/knockout/revivePlayer.js
@@ -3,10 +3,13 @@ function revivePlayer(npc){
     var potionIds = JSON.parse(player.getStoredData('potionIds'));
     var potionAmps = JSON.parse(player.getStoredData('potionAmps'));
     var potionDurs = JSON.parse(player.getStoredData('potionDurs'));
+    npc.setVisibleType(1);
     player.setPosition(npc.getPosition());
     player.sendMessage('Revived! ' + player.getName());
     for (var i = 0; i < potionIds.length; ++i){
         player.addPotionEffect(potionIds[i],potionDurs[i],potionAmps[i],true);
     }
+    npc.setRespawnCycle(3);
+    npc.hideKilledBody(1);
     npc.kill();
 }
